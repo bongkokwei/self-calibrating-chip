5158d35d12837cf4b0af172afa4b4933dd0af28c
Kok-Wei Bong <bongkokwei@gmail.com>
2026-01-09 09:32:03 +1100

    added function to test instr conn

--- experiment/utils/__init__.py
+++ experiment/utils/__init__.py
@@ -4,3 +4,8 @@ Useful scripts that controls the experiment's instruments
 
 from .measure_spectrum import measure_and_save_spectrum
 from .extract_tap_coefficients import extract_tap_coefficients
+from .test_instruments import (
+    configure_edfa,
+    configure_voltage_controller,
+    measure_with_ova,
+)
